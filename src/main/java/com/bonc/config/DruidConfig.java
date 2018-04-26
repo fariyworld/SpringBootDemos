94d1a9a8b7f6a55a5fe4be396a5714b7c696f2ff
mace <1525752742@qq.com>
2018-04-26 08:50:24 +0800

    springboot 远程部署配置，druid监控ip配置

--- src/main/java/com/bonc/config/DruidConfig.java
+++ src/main/java/com/bonc/config/DruidConfig.java
@@ -22,12 +22,14 @@ public class DruidConfig {
 
         //添加初始化参数：initParams
         //白名单：
-        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+//        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+//        servletRegistrationBean.addInitParameter("allow","192.168.88.128");
+//        servletRegistrationBean.addInitParameter("allow","172.16.92.5");
         //IP黑名单 (存在共同时，deny优先于allow) : 如果满足deny的话提示:Sorry, you are not permitted to view this page.
         //servletRegistrationBean.addInitParameter("deny","192.168.1.73");
         //登录查看信息的账号密码.
-        servletRegistrationBean.addInitParameter("loginUsername","admin");
-        servletRegistrationBean.addInitParameter("loginPassword","admin");
+        servletRegistrationBean.addInitParameter("loginUsername","mace");
+        servletRegistrationBean.addInitParameter("loginPassword","liuye0425");
         //是否能够重置数据.
         servletRegistrationBean.addInitParameter("resetEnable","false");
         return servletRegistrationBean;
