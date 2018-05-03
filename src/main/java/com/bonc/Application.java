3a68bc287b636bbc847c1b710928bbde94f078db
mace <1525752742@qq.com>
2018-05-03 10:12:24 +0800

    修改ip

--- src/main/java/com/bonc/Application.java
+++ src/main/java/com/bonc/Application.java
@@ -3,13 +3,11 @@ package com.bonc;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * @Description: SpringBoot启动Application
