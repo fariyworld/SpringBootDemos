efdea7db90b305b4272fdf75db14b1be11e1326b
mace <1525752742@qq.com>
2018-05-19 10:48:51 +0800

    yml配置文件实现session共享或者代码实现 RedisHttpSessionConfig.java //@Configuration //@EnableRedisHttpSession(redisNamespace = "springboot-session-redis")//开启spring session支持

---
+++ src/main/java/com/bonc/config/RedisHttpSessionConfig.java
@@ -0,0 +1,12 @@
+package com.bonc.config;
+
+/**
+ * description:
+ * <br />
+ * Created by mace on 17:25 2018/5/18.
+ */
+//@Configuration
+//@EnableRedisHttpSession(redisNamespace = "springboot-session-redis")//开启spring session支持
+public class RedisHttpSessionConfig {
+
+}
